b3689a0fb21931fab4a7be09f71364230e1d6501
Alan Ciampaglia <alan.ciampaglia@ringtailinteractive.com>
2023-11-04 22:29:42 +0100

    web test

---
+++ main.js
@@ -0,0 +1,13 @@
+//import { dotnet } from "./_framework/dotnet.js";
+
+//await dotnet
+//    //.withDebugging(1)
+//    .withDiagnosticTracing(false)
+//    //.withApplicationArgumentsFromQuery()
+//    .create();
+
+//dotnet.instance.Module['canvas'] = (function () {
+//    return document.getElementById('canvas');
+//})();
+
+//await dotnet.run();
